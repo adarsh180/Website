1fda1471cfb71c2c9c2d4d64d8fc6762aeb115cb
unknown <tiwariadarsh1804@gmail.com>
2024-02-11 09:51:48 +0530

    Ecommerce

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navigation from "./customer/components/Navigation/Navigation.jsx";
+
+function App() {
+  return (
+    <div className=" ">
+    <Navigation />
+    <div>
+     home
+    </div>
+    </div>
+  );
+}
+
+export default App;
